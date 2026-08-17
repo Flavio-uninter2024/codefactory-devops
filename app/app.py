3d190e8f066c2e26a575b84743cc75644b1e80b0
Flavio Nascimento <flavioborges131194@hotmail.com>
2026-08-16 22:41:53 -0300

    feat: cria aplicação inicial CodeFactory

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "projeto": "CodeFactory DevOps",
+        "mensagem": "Aplicação funcionando!",
+        "status": "online"
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
